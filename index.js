21f6e31450d293e4f9413f93083ae8bd2959dc28
Katy DeCorah <katydecorah@gmail.com>
2020-01-03 21:07:48 -0500

    :heavy_plus_sign: Alt text should be fewer than 100 characters

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function checker(altText) {
   );
   // check if alt text ends with a  period
   if (!altText.endsWith(".")) warning.push("Alt text should end in a period.");
+  // check if alt text length is > 100
+  if (altText.length > 100)
+    warning.push("Alt text should be fewer than 100 characters.");
   // if warning has length, return it.
   return warning.length ? warning.join(" ") : undefined;
 }
--- tests/checker.test.js
+++ tests/checker.test.js
@@ -20,5 +20,12 @@ test("checker", assert => {
 
   assert.equal(checker("A large black dog."), undefined);
 
+  assert.equal(
+    checker(
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    "Alt text should be fewer than 100 characters."
+  );
+
   assert.end();
 });
