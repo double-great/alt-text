f95c941a9a0737878eef8d3bbb6a596207d6bfea
Katy DeCorah <katydecorah@gmail.com>
2020-01-03 21:04:50 -0500

    💬 add comments

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const forbidden = require("./lib/forbidden");
 
 function checker(altText) {
-  let warning = forbidden.reduce((arr, item) => {
-    if (altText.includes(item))
-      arr.push(`Alt text should not contain "${item}".`);
-    return arr;
-  }, []);
+  // store warning for the alt text
+  let warning = [];
+  // check if alt text matches any item in forbidden
+  warning = warning.concat(
+    forbidden.reduce((arr, item) => {
+      if (altText.includes(item))
+        arr.push(`Alt text should not contain "${item}".`);
+      return arr;
+    }, [])
+  );
+  // check if alt text ends with a  period
   if (!altText.endsWith(".")) warning.push("Alt text should end in a period.");
+  // if warning has length, return it.
   return warning.length ? warning.join(" ") : undefined;
 }
 
--- tests/checker.test.js
+++ tests/checker.test.js
@@ -6,10 +6,19 @@ test("checker", assert => {
     checker("A screenshot of a dog."),
     'Alt text should not contain "screenshot of".'
   );
+
+  assert.equal(
+    checker("A screenshot of a dog"),
+    'Alt text should not contain "screenshot of". Alt text should end in a period.',
+    "can have more than one warning"
+  );
+
   assert.equal(
     checker("a large black dog"),
     "Alt text should end in a period."
   );
+
   assert.equal(checker("A large black dog."), undefined);
+
   assert.end();
 });
