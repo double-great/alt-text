311e0a6cedb97cd076806814e2b0616ef17fb243
Katy DeCorah <katydecorah@gmail.com>
2020-01-03 20:50:56 -0500

    :heavy_plus_sign: alt text should end in a period

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const forbidden = require("./lib/forbidden");
 
 function checker(altText) {
-  let warning = forbidden.reduce((string, item) => {
+  let warning = forbidden.reduce((arr, item) => {
     if (altText.includes(item))
-      string += `Alt text should not contain "${item}".`;
-    return string;
-  }, "");
-  return warning;
+      arr.push(`Alt text should not contain "${item}".`);
+    return arr;
+  }, []);
+  if (!altText.endsWith(".")) warning.push("Alt text should end in a period.");
+  return warning.length ? warning.join(" ") : undefined;
 }
 
 module.exports = checker;
--- tests/checker.test.js
+++ tests/checker.test.js
@@ -3,9 +3,13 @@ const checker = require("../");
 
 test("checker", assert => {
   assert.equal(
-    checker("a screenshot of a dog"),
+    checker("A screenshot of a dog."),
     'Alt text should not contain "screenshot of".'
   );
-  assert.equal(checker("a large black dog"), "");
+  assert.equal(
+    checker("a large black dog"),
+    "Alt text should end in a period."
+  );
+  assert.equal(checker("A large black dog."), undefined);
   assert.end();
 });
