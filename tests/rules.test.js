7a4bb069c28ea7fec6df773bb8c0f795aa9da2e7
Katy DeCorah <katydecorah@gmail.com>
2020-01-05 20:26:17 -0500

    :wrench: provide current alt text length in warning (#18)

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -57,7 +57,9 @@ test("[rules] checkLength", assert => {
     rules.checkLength(
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     ),
-    ["Alt text should be fewer than 100 characters."]
+    [
+      "Alt text length should be less than 100 characters, it is currently 446 characters."
+    ]
   );
   assert.end();
 });
