5e1a0815fce66e228557004dde4e84e05d6e36ac
Katy DeCorah <katydecorah@gmail.com>
2020-01-25 20:49:43 -0500

    make sources a list, clean up

--- scripts/write-docs.js
+++ scripts/write-docs.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const clues = require("../clues");
 
 const md = Object.keys(clues).reduce((str, clue) => {
-  const options = clues[clue].rules ? `<${clues[clue].rules.join("|")}>` : "";
-
-  str += `### ${clues[clue].heading}\n\n`;
-  str += `Warning: \`${clues[clue].message(options)}\``;
-
+  const { message, rationale, source, heading, rules } = clues[clue];
+  const options = rules ? `<${rules.join("|")}>` : "";
+  str += `### ${heading}\n\n`;
+  str += `Warning: \`${message(options)}\``;
   str += "\n\n";
-  if (clues[clue].rationale) str += `${clues[clue].rationale}\n\n`;
-  if (clues[clue].source) str += `Source: <${clues[clue].source}>\n\n`;
+  if (rationale) str += `${rationale}\n\n`;
+  if (source)
+    str += `Sources:\n${source.map(link => `- <${link}>\n`).join("")}\n`;
   return str;
 }, "## Warnings\n\n<!-- this section is generated on commit !-->\n\n");
 
