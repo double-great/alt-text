e59670f1d4d30c09803a7d57479413c825fa342d
Katy DeCorah <katydecorah@gmail.com>
2020-01-26 14:05:19 -0500

    uncomment source, rationale tests (#35)
    Co-authored-by: Jason Morris <jasonmorris@gmail.com>

--- tests/clues.test.js
+++ tests/clues.test.js
@@ -18,14 +18,13 @@ Object.keys(clues).forEach(clue => {
       false,
       "`warning` must not end in period"
     );
-    /*assert.ok(
-      clues[clue].source,
-      "must have `source`"
+    assert.ok(clues[clue].source, "must have `source`");
+    assert.equal(
+      typeof clues[clue].source,
+      "object",
+      "source must be an array"
     );
-    assert.ok(
-      clues[clue].rationale,
-      "must have `rationale`"
-    );*/
+    assert.ok(clues[clue].rationale, "must have `rationale`");
     assert.ok(clues[clue].ok, "must have `ok`");
     assert.ok(clues[clue].notOk, "must have `notOk`");
     assert.ok(clues[clue].heading, "must have `heading`");
