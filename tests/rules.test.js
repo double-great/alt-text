f0bd0d1a5d9737dc52a6a0acbb43dc6822124ca7
Katy DeCorah <katydecorah@gmail.com>
2021-12-30 11:35:30 -0500

    Switch to Jest

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -1,84 +1,108 @@
-import test from "tape";
-import { checkClue, checkPunctuation, checkLength, checkOnlySpace, checkEmoji} from "../rules.js";
+import {
+  checkClue,
+  checkPunctuation,
+  checkLength,
+  checkOnlySpace,
+  checkEmoji,
+} from "../rules.js";
 
-test("[rules] checkClue.endWith", (assert) => {
-  assert.deepEqual(
-    checkClue("A screenshot of a dog.jpg"),
-    [
-      'Alt text should not contain "screenshot of" (https://git.io/JvqAM).',
-      'Alt text should not end with ".jpg" (https://git.io/JvfAf).',
-    ],
-    "can have more than one suggestion"
-  );
-  assert.deepEqual(checkClue("DSC_0010.jpg"), [
-    'Alt text should not end with ".jpg" (https://git.io/JvfAf).',
-  ]);
-  assert.deepEqual(checkClue("placeholder graphic"), [
-    'Alt text should not end with "graphic" (https://git.io/JvfAf).',
-  ]);
-  assert.end();
-});
+describe("rules", () => {
+  test("checkClue.endWith", () => {
+    expect(checkClue("A screenshot of a dog.jpg")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not contain \\"screenshot of\\" (https://git.io/JvqAM).",
+        "Alt text should not end with \\".jpg\\" (https://git.io/JvfAf).",
+      ]
+    `);
+    expect(checkClue("DSC_0010.jpg")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not end with \\".jpg\\" (https://git.io/JvfAf).",
+      ]
+    `);
+    expect(checkClue("placeholder graphic")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not end with \\"graphic\\" (https://git.io/JvfAf).",
+      ]
+    `);
+  });
 
-test("[rules] checkClue.startWith", (assert) => {
-  assert.deepEqual(checkClue("spacer image."), [
-    'Alt text should not start with "spacer" (https://git.io/JvfAv).',
-  ]);
-  assert.end();
-});
+  test("checkClue.startWith", () => {
+    expect(checkClue("spacer image.")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not start with \\"spacer\\" (https://git.io/JvfAv).",
+      ]
+    `);
+  });
 
-test("[rules] checkClue.exactMatch", (assert) => {
-  assert.deepEqual(checkClue("logo"), [
-    'Alt text should not be "logo" (https://git.io/JvqAK).',
-  ]);
-  assert.end();
-});
+  test("checkClue.exactMatch", () => {
+    expect(checkClue("logo")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not be \\"logo\\" (https://git.io/JvqAK).",
+      ]
+    `);
+  });
 
-test("[rules] checkClue.exactMatch", (assert) => {
-  assert.deepEqual(checkClue(" logo "), [
-    'Alt text should not be "logo" (https://git.io/JvqAK).',
-  ]);
-  assert.end();
-});
+  test("checkClue.exactMatch", () => {
+    expect(checkClue(" logo ")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not be \\"logo\\" (https://git.io/JvqAK).",
+      ]
+    `);
+  });
 
-test("[rules] checkClue.contain", (assert) => {
-  assert.deepEqual(checkClue("A screenshot of a dog."), [
-    'Alt text should not contain "screenshot of" (https://git.io/JvqAM).',
-  ]);
-  assert.end();
-});
+  test("checkClue.contain", () => {
+    expect(checkClue("A screenshot of a dog.")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not contain \\"screenshot of\\" (https://git.io/JvqAM).",
+      ]
+    `);
+  });
 
-test("[rules] checkPunctuation", (assert) => {
-  assert.deepEqual(checkPunctuation("a large black dog"), [
-    "Alt text should end with punctuation (https://git.io/JJk55).",
-  ]);
-  assert.deepEqual(checkPunctuation("a large black dog."), []);
-  assert.deepEqual(checkPunctuation("a large black dog!"), []);
-  assert.deepEqual(checkPunctuation("a large black dog?"), []);
-  assert.end();
-});
+  test("checkPunctuation", () => {
+    expect(checkPunctuation("a large black dog")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should end with punctuation (https://git.io/JJk55).",
+      ]
+    `);
+    expect(checkPunctuation("a large black dog.")).toMatchInlineSnapshot(
+      [],
+      `Array []`
+    );
+    expect(checkPunctuation("a large black dog!")).toMatchInlineSnapshot(
+      [],
+      `Array []`
+    );
+    expect(checkPunctuation("a large black dog?")).toMatchInlineSnapshot(
+      [],
+      `Array []`
+    );
+  });
 
-test("[rules] checkLength", (assert) => {
-  assert.deepEqual(
-    checkLength(
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    ),
-    [
-      "Alt text length should be less than 125 characters, it is currently 446 characters (https://git.io/Jvfxm).",
-    ]
-  );
-  assert.end();
-});
+  test("checkLength", () => {
+    expect(
+      checkLength(
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      )
+    ).toMatchInlineSnapshot(`
+      Array [
+        "Alt text length should be less than 125 characters, it is currently 446 characters (https://git.io/Jvfxm).",
+      ]
+    `);
+  });
 
-test("[rules] checkOnlySpace", (assert) => {
-  assert.deepEqual(checkOnlySpace(" "), [
-    "Alt text should not be a single space (https://git.io/Jvqim).",
-  ]);
-  assert.end();
-});
+  test("checkOnlySpace", () => {
+    expect(checkOnlySpace(" ")).toMatchInlineSnapshot(`
+      Array [
+        "Alt text should not be a single space (https://git.io/Jvqim).",
+      ]
+    `);
+  });
 
-test("[rules] checkEmoji", (assert) => {
-  assert.deepEqual(checkEmoji("An orange 🐈."), [
-    "Replace 🐈 in alt text with descriptive text (https://git.io/Jfhte).",
-  ]);
-  assert.end();
+  test("checkEmoji", () => {
+    expect(checkEmoji("An orange 🐈.")).toMatchInlineSnapshot(`
+      Array [
+        "Replace 🐈 in alt text with descriptive text (https://git.io/Jfhte).",
+      ]
+    `);
+  });
 });
