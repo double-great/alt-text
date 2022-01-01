00c74414ad4cc8d54fb6843a17293a7f0868ce34
Katy DeCorah <katydecorah@gmail.com>
2022-01-01 15:04:33 -0500

    Switch to Jest (#165)

--- tests/clues.test.js
+++ tests/clues.test.js
@@ -1,62 +1,78 @@
-import test from "tape";
 import clues from "../clues.js";
 
-test("[clues]", (assert) => {
-  assert.equal(typeof clues, "object", "shape of clues is an object");
-  assert.end();
-});
-
-Object.keys(clues).forEach((clue) => {
-  test(`[clues] ${clue}`, (assert) => {
-    assert.equal(
-      typeof clues[clue].suggestion,
-      "function",
-      "value of `suggestion` is a function"
-    );
-    assert.equal(
-      clues[clue].suggestion().endsWith("."),
-      false,
-      "`suggestion` must not end in period"
-    );
-    assert.ok(clues[clue].source, "must have `source`");
-    assert.equal(
-      typeof clues[clue].source,
-      "object",
-      "source must be an array"
-    );
-    assert.ok(clues[clue].rationale, "must have `rationale`");
-    assert.ok(clues[clue].ok, "must have `ok`");
-    assert.ok(clues[clue].notOk, "must have `notOk`");
-    assert.ok(clues[clue].heading, "must have `heading`");
-    if (clues[clue].fn) {
-      assert.equal(
-        typeof clues[clue].fn,
-        "function",
-        "value of `fn` is a function"
-      );
-    }
-    if (clues[clue].rules) {
-      assert.equal(
-        typeof clues[clue].rules,
-        "object",
-        "value of `rules` is an object (array)"
-      );
-      clues[clue].rules.forEach((rule) => {
-        assert.equal(rule, rule.toLowerCase(), `"${rule}" must be lowercase`);
+describe("clues", () => {
+  it("shape of clues is an object", () => {
+    expect(typeof clues).toEqual("object");
+  });
+  Object.keys(clues).forEach((clue) => {
+    describe(`clue: ${clue}`, () => {
+      const {
+        suggestion,
+        source,
+        rationale,
+        ok,
+        notOk,
+        heading,
+        fn,
+        rules,
+        listen,
+      } = clues[clue];
+      it("value of `suggestion` is a function", () => {
+        expect(typeof suggestion).toEqual("function");
       });
-    }
-    if (clues[clue].listen) {
-      assert.equal(
-        typeof clues[clue].listen,
-        "string",
-        "`listen` must be a string"
-      );
-      assert.ok(
-        clues[clue].listen.startsWith("https://"),
-        "`listen` must start with 'https://'"
-      );
-    }
 
-    assert.end();
+      it("`suggestion` must not end in period", () => {
+        expect(suggestion().endsWith(".")).toBeFalsy();
+      });
+
+      it("must have `source`", () => {
+        expect(source).toBeDefined();
+      });
+
+      it("source must be an array", () => {
+        expect(typeof source).toEqual("object");
+      });
+
+      it("must have `rationale`", () => {
+        expect(rationale).toBeDefined();
+      });
+
+      it("must have `ok`", () => {
+        expect(ok).toBeDefined();
+      });
+
+      it("must have `notOk`", () => {
+        expect(notOk).toBeDefined();
+      });
+      it("must have `heading`", () => {
+        expect(heading).toBeDefined();
+      });
+
+      if (fn) {
+        it("value of `fn` is a function", () => {
+          expect(typeof fn).toEqual("function");
+        });
+      }
+      if (rules) {
+        it("value of `rules` is an object (array)", () => {
+          expect(typeof rules).toEqual("object");
+        });
+
+        rules.forEach((rule) => {
+          it(`"${rule}" must be lowercase`, () => {
+            expect(rule).toEqual(rule.toLowerCase());
+          });
+        });
+      }
+      if (listen) {
+        it("`listen` must be a string", () => {
+          expect(typeof listen).toEqual("string");
+        });
+
+        it("`listen` must start with 'https://'", () => {
+          expect(listen.startsWith("https://")).toBeTruthy();
+        });
+      }
+    });
   });
 });
