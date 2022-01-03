893fa1d8ffc4b9b1b64824b1edfd1fe6e4881894
Katy DeCorah <katydecorah@gmail.com>
2022-01-03 13:09:41 -0500

    Create tests for write-docs (#168)

---
+++ tests/write-docs.test.js
@@ -0,0 +1,9 @@
+import { formatDocs, swapDocs } from "../scripts/write-docs-utils.js";
+import clues from "../clues.js";
+
+describe("Documentation in README.md is up-to-date", () => {
+  it("Run `npm run write-docs` to update documentation", () => {
+    const { matchedDocs } = swapDocs();
+    expect(formatDocs(clues)).toEqual(matchedDocs[0]);
+  });
+});
