4419555812a0249c0a3a58dd25e493e46cf483a3
Katy DeCorah <katydecorah@gmail.com>
2022-01-03 13:45:41 -0500

    Remove tests that were made redundant by TypeScript

--- tests/clues.test.ts
+++ tests/clues.test.ts
@@ -16,56 +16,11 @@ async function checkDocsLink(clue) {
 }
 
 describe("clues", () => {
-  it("shape of clues is an object", () => {
-    expect(typeof clues).toEqual("object");
-  });
   Object.keys(clues).forEach((clue) => {
     describe(`clue: ${clue}`, () => {
-      const {
-        suggestion,
-        source,
-        rationale,
-        ok,
-        notOk,
-        heading,
-        fn,
-        rules,
-        listen,
-        docs,
-      } = clues[clue];
-      it("value of `suggestion` is a function", () => {
-        expect(typeof suggestion).toEqual("function");
-      });
-
+      const { suggestion, rules, listen, docs } = clues[clue];
       it("`suggestion` must not end in period", () => {
         expect(suggestion().endsWith(".")).toBeFalsy();
-      });
-
-      it("must have `source`", () => {
-        expect(source).toBeDefined();
-      });
-
-      it("source must be an array", () => {
-        expect(typeof source).toEqual("object");
-      });
-
-      it("must have `rationale`", () => {
-        expect(rationale).toBeDefined();
-      });
-
-      it("must have `ok`", () => {
-        expect(ok).toBeDefined();
-      });
-
-      it("must have `notOk`", () => {
-        expect(notOk).toBeDefined();
-      });
-      it("must have `heading`", () => {
-        expect(heading).toBeDefined();
-      });
-
-      it("must have `docs`", () => {
-        expect(docs).toBeDefined();
       });
 
       it("`docs` matches generated GitHub `heading` link", async () => {
@@ -73,16 +28,7 @@ describe("clues", () => {
         await expect(checkDocsLink(clues[clue])).resolves.toEqual(docs);
       });
 
-      if (fn) {
-        it("value of `fn` is a function", () => {
-          expect(typeof fn).toEqual("function");
-        });
-      }
       if (rules) {
-        it("value of `rules` is an object (array)", () => {
-          expect(typeof rules).toEqual("object");
-        });
-
         rules.forEach((rule) => {
           it(`"${rule}" must be lowercase`, () => {
             expect(rule).toEqual(rule.toLowerCase());
@@ -90,10 +36,6 @@ describe("clues", () => {
         });
       }
       if (listen) {
-        it("`listen` must be a string", () => {
-          expect(typeof listen).toEqual("string");
-        });
-
         it("`listen` must start with 'https://'", () => {
           expect(listen.startsWith("https://")).toBeTruthy();
         });
