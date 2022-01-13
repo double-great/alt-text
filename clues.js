68f095e6c64347d16a1babfcb44701341b1bfd36
Katy DeCorah <katydecorah@gmail.com>
2022-01-12 20:18:27 -0500

    Switch to tinyurl (#174)

--- clues.js
+++ clues.js
@@ -1,7 +1,7 @@
 export default {
   notOnlySpace: {
     heading: "Empty alt text",
-    docs: "https://git.io/Jvqim",
+    docs: "https://tinyurl.com/y2o7uctu",
     suggestion: () => `Alt text should not be a single space`,
     rationale:
       'If you use null (empty) alt text (`alt=""`) to hide decorative images, make sure that there is no space character in between the quotes. **If a space character is present, the image may not be effectively hidden from assistive technologies.** For instance, some screen readers will still announce the presence of an image if a space character is put between the quotes.',
@@ -11,7 +11,7 @@ export default {
   },
   charLength: {
     heading: "Character length",
-    docs: "https://git.io/Jvfxm",
+    docs: "https://tinyurl.com/y2f7rhao",
     suggestion: (length) =>
       `Alt text length should be less than 125 characters${
         length ? `, it is currently ${length} characters` : ""
@@ -28,7 +28,7 @@ export default {
   },
   imageLink: {
     heading: "Image is link",
-    docs: "https://git.io/JvfNj",
+    docs: "https://tinyurl.com/yxhq2k5w",
     suggestion: () =>
       `Images inside a link tag require alt text that describes the purpose of the link`,
     rationale:
@@ -40,7 +40,7 @@ export default {
   },
   endPunctuation: {
     heading: "End with punctuation",
-    docs: "https://git.io/JJk55",
+    docs: "https://tinyurl.com/y5krn3ny",
     suggestion: () => `Alt text should end with punctuation`,
     rationale:
       "End the alt text with a period, exclamation point, or question mark. This will make screen readers pause a bit after the last word in the alt text, which creates a more pleasant reading experience for the user.",
@@ -51,7 +51,7 @@ export default {
   },
   noAlt: {
     heading: "Missing alt attribute",
-    docs: "https://git.io/JvfNh",
+    docs: "https://tinyurl.com/yybc6bsy",
     suggestion: () => `Missing "alt" attribute`,
     rationale:
       "All images must have alternate text to convey their purpose and meaning to screen reader users.",
@@ -61,7 +61,7 @@ export default {
   },
   contains: {
     heading: "Alt text contains unhelpful words",
-    docs: "https://git.io/JvqAM",
+    docs: "https://tinyurl.com/y3v3jgux",
     fn: (item, alt) => alt.includes(item),
     suggestion: (value) => `Alt text should not contain "${value}"`,
     rules: [
@@ -86,7 +86,7 @@ export default {
   },
   exactMatch: {
     heading: "Alt text is unhelpful",
-    docs: "https://git.io/JvqAK",
+    docs: "https://tinyurl.com/yxwc2hof",
     fn: (item, alt) => item == alt.trim(),
     suggestion: (value) => `Alt text should not be "${value}"`,
     rules: [
@@ -128,7 +128,7 @@ export default {
   },
   endWith: {
     heading: "Alt text should not end with",
-    docs: "https://git.io/JvfAf",
+    docs: "https://tinyurl.com/yy2q8bbb",
     fn: (item, alt) => alt.endsWith(item),
     suggestion: (value) => `Alt text should not end with "${value}"`,
     rules: [
@@ -148,7 +148,7 @@ export default {
   },
   startWith: {
     heading: "Alt text should not start with",
-    docs: "https://git.io/JvfAv",
+    docs: "https://tinyurl.com/y5y98ygu",
     fn: (item, alt) => alt.startsWith(item),
     suggestion: (value) => `Alt text should not start with "${value}"`,
     rules: [
@@ -173,7 +173,7 @@ export default {
   },
   decorative: {
     heading: "Image is decorative",
-    docs: "https://git.io/Jvqx8",
+    docs: "https://tinyurl.com/yxnvejgv",
     suggestion: () =>
       `Empty alt text should only be used for decorative images`,
     rationale: `Provide "null" \`alt\` attributes (using \`alt=""\`) for images which do not provide information or do not require alternative text because the image is described in the page content. Some developers will mistakenly leave off the alt attribute altogether on images which they deem do not need alternatives. This is not helpful to assistive technology users because the assistive technology, such as screen reader, will often read the source attribute (i.e., file name) as the alternative text. To tell assistive technology to ignore an image, use a "blank alternative text" attribute: \`alt=""\`.`,
@@ -183,7 +183,7 @@ export default {
   },
   avoidEmoji: {
     heading: "Avoid emoji",
-    docs: "https://git.io/Jfhte",
+    docs: "https://tinyurl.com/yylrxrus",
     suggestion: (emoji) =>
       `Replace ${emoji || "emoji"} in alt text with descriptive text`,
     rationale: `Emoji have their own text descriptions. These descriptions can vary between operating systems and software. The spoken description of the emoji may not match your visual intention.`,
--- tests/index.test.js
+++ tests/index.test.js
@@ -11,30 +11,30 @@ describe("altText", () => {
 
   test("undefined", () => {
     expect(altText()).toMatchInlineSnapshot(
-      `"Empty alt text should only be used for decorative images (https://git.io/Jvqx8)."`
+      `"Empty alt text should only be used for decorative images (https://tinyurl.com/yxnvejgv)."`
     );
     expect(altText("")).toMatchInlineSnapshot(
-      `"Empty alt text should only be used for decorative images (https://git.io/Jvqx8)."`
+      `"Empty alt text should only be used for decorative images (https://tinyurl.com/yxnvejgv)."`
     );
     expect(altText(null)).toMatchInlineSnapshot(
-      `"Empty alt text should only be used for decorative images (https://git.io/Jvqx8)."`
+      `"Empty alt text should only be used for decorative images (https://tinyurl.com/yxnvejgv)."`
     );
   });
 
   test("return suggestions", () => {
     expect(altText("A SCREENSHOT OF A DOG.")).toMatchInlineSnapshot(
-      `"Alt text should not contain \\"screenshot of\\" (https://git.io/JvqAM)."`
+      `"Alt text should not contain \\"screenshot of\\" (https://tinyurl.com/y3v3jgux)."`
     );
     expect(altText("A SCREENSHOT OF A DOG")).toMatchInlineSnapshot(
-      `"Alt text should not contain \\"screenshot of\\" (https://git.io/JvqAM). Alt text should end with punctuation (https://git.io/JJk55)."`
+      `"Alt text should not contain \\"screenshot of\\" (https://tinyurl.com/y3v3jgux). Alt text should end with punctuation (https://tinyurl.com/y5krn3ny)."`
     );
     expect(
       altText("An inhaler with a spacer connected to the mouthpiece")
     ).toMatchInlineSnapshot(
-      `"Alt text should end with punctuation (https://git.io/JJk55)."`
+      `"Alt text should end with punctuation (https://tinyurl.com/y5krn3ny)."`
     );
     expect(altText("😸.")).toMatchInlineSnapshot(
-      `"Replace 😸 in alt text with descriptive text (https://git.io/Jfhte)."`
+      `"Replace 😸 in alt text with descriptive text (https://tinyurl.com/yylrxrus)."`
     );
   });
 });
