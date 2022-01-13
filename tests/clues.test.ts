bb6c1e549cf5aae73116c48e3fe48047bceb3710
Katy DeCorah <katydecorah@gmail.com>
2022-01-13 10:31:09 -0500

    Clean up

--- tests/clues.test.ts
+++ tests/clues.test.ts
@@ -4,28 +4,28 @@ import GithubSlugger from "github-slugger";
 
 const slugger = new GithubSlugger();
 
-const getUrl = (url) =>
+const getUrl = (url: string) =>
   new Promise((resolve) =>
-    TinyUrl.shorten(url, (shortURL) => resolve(shortURL))
+    TinyUrl.shorten(url, (shortURL: string) => resolve(shortURL))
   );
 
-async function checkDocsLink(clue) {
+async function checkDocsLink(heading: string) {
   return await getUrl(
-    `https://github.com/double-great/alt-text#${slugger.slug(clue.heading)}`
+    `https://github.com/double-great/alt-text#${slugger.slug(heading)}`
   );
 }
 
 describe("clues", () => {
   Object.keys(clues).forEach((clue) => {
     describe(`clue: ${clue}`, () => {
-      const { suggestion, rules, listen, docs } = clues[clue];
+      const { suggestion, rules, listen, docs, heading } = clues[clue];
       it("`suggestion` must not end in period", () => {
         expect(suggestion().endsWith(".")).toBeFalsy();
       });
 
       it("`docs` matches generated GitHub `heading` link", async () => {
         expect.assertions(1);
-        await expect(checkDocsLink(clues[clue])).resolves.toEqual(docs);
+        await expect(checkDocsLink(heading)).resolves.toEqual(docs);
       });
 
       if (rules) {
